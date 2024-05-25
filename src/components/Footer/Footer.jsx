cf06a9502ca6618d99109e0a1287763e998aa29e
Daniel Amaya <danijcdm.com@gmail.com>
2024-05-25 12:41:38 -0500

    Frontend

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -48,9 +48,9 @@ const Footer = () => {
                         <p><a href="#" className="text-white">Mi cuenta</a></p>
                         <h5 className="mt-4 mb-3">Síguenos en:</h5>
                         <div className="d-flex">
-                        <img src="../../img/icon-facebook.png" alt="Facebook" style={{width: '30px', marginRight: '10px'}} />
-<img src="../../img/icon-instagram.png" alt="Instagram" style={{width: '30px', marginRight: '10px'}} />
-<img src="../../img/icon-twitter.png" alt="Twitter" style={{width: '30px', marginRight: '10px'}} />
+                        <img src="../img/icon-facebook.png" alt="Facebook" style={{width: '30px', marginRight: '10px'}} />
+                        <img src="../../img/icon-instagram.png" alt="Instagram" style={{width: '30px', marginRight: '10px'}} />
+                        <img src="../../img/icon-twitter.png" alt="Twitter" style={{width: '30px', marginRight: '10px'}} />
 
                             {/* <a href="https://api.whatsapp.com/send?phone=573173026445&text=%C2%A1Hola%20Tienda%20Mac!%20"><img src="src/img/icon-whatsApp.png" alt="WhatsApp" style={{width: '30px', marginRight: '10px'}} /></a> */}
                         </div>
