903e25031678c249aeed215ec0a6abcee5e26b17
Daniel Amaya <danijcdm.com@gmail.com>
2024-05-31 00:13:07 -0500

    30/05/2024

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -4,7 +4,6 @@ import ProductForm from '../Product/ProductForm';
 import ProductUpdate from '../Product/ProductUpdate';
 import ProductDetail from './ProductDetail';
 import MenuDashboard from '../MenuDashboard/MenuDashboard';
-import Pagination from '../Paginado/Pagination'; // Importa el componente Pagination
 
 const Product = () => {
     const [showProductForm, setShowProductForm] = useState(false);
@@ -12,12 +11,10 @@ const Product = () => {
     const [editProductId, setEditProductId] = useState(null);
     const [selectedProductId, setSelectedProductId] = useState(null);
     const [selectedProduct, setSelectedProduct] = useState(null);
-    const [currentPage, setCurrentPage] = useState(1); // Estado para la página actual
-    const [itemsPerPage, setItemsPerPage] = useState(10); // Estado para la cantidad de elementos por página
 
     useEffect(() => {
         fetchProducts();
-    }, [currentPage, itemsPerPage]); // Actualiza la lista de productos cuando cambia la página o la cantidad de elementos por página
+    }, []);
 
     useEffect(() => {
         if (selectedProductId) {
@@ -29,18 +26,12 @@ const Product = () => {
 
     const fetchProducts = async () => {
         try {
-            const offset = (currentPage - 1) * itemsPerPage;
-            const response = await axios.get(`https://backend-tienda-mac-production.up.railway.app/product?_page=${currentPage}&_limit=${itemsPerPage}`);
-            if (response.data && Array.isArray(response.data)) {
-                setProducts(response.data);
-            } else {
-                console.error('Error fetching products: response data is not an array');
-            }
+            const response = await axios.get('https://backend-tienda-mac-production.up.railway.app/product');
+            setProducts(response.data);
         } catch (error) {
             console.error('Error fetching products:', error);
         }
     };
-    
 
     const fetchProduct = async (productId) => {
         try {
@@ -88,10 +79,9 @@ const Product = () => {
 
     return (
         <>
-            
             <div className="container-fluid">
                 <div className="row">
-                    <MenuDashboard />     
+                    <MenuDashboard />
                     <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                         <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                             <button className="btn btn-primary" onClick={() => setShowProductForm(!showProductForm)}>
@@ -102,7 +92,7 @@ const Product = () => {
                         {editProductId && <ProductUpdate productId={editProductId} onClose={handleCloseEdit} />}
                         {selectedProduct && <ProductDetail product={selectedProduct} />}
                         <h2>Lista de Productos</h2>
-                        <hr></hr>
+                        <hr />
                         <div className="table-responsive">
                             <table className="table table-striped">
                                 <thead>
@@ -111,7 +101,7 @@ const Product = () => {
                                         <th>Nombre</th>
                                         <th>Precio</th>
                                         <th>Precio USD</th>
-                                        <th>Cantidad</th>         
+                                        <th>Cantidad</th>
                                         <th>Garantía</th>
                                         <th>Moneda</th>
                                         <th>Código de barras</th>
@@ -139,15 +129,6 @@ const Product = () => {
                                 </tbody>
                             </table>
                         </div>
-                        <Pagination
-                          totalItems={products.length} // Puedes usar products.length si no tienes la cantidad total de productos disponible
-                          itemsPerPageOptions={[5, 10, 20, 30, 40, 50]}
-                          defaultItemsPerPage={10}
-                          onPageChange={(page, itemsPerPage) => {
-                              setCurrentPage(page);
-                              setItemsPerPage(itemsPerPage);
-                          }}
-                        />
                     </main>
                 </div>
             </div>
