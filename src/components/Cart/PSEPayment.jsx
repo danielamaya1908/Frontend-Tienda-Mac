54054cc70699c727a16148befb036a7e64a705e7
Daniel Amaya <danijcdm.com@gmail.com>
2024-08-03 05:07:39 -0500

    Frontend-prueba-pasarela-5

--- src/components/Cart/PSEPayment.jsx
+++ src/components/Cart/PSEPayment.jsx
@@ -45,7 +45,7 @@ const PSEPayment = () => {
         customer: customerData,
         confirm: 'false',
         send_email: 'true',
-        redirect_url: 'https://backend-tienda-mac-production.up.railway.app/api/openpay/payment-confirmation'
+        redirect_url: 'https://www.tiendamac.net/payment-confirmation' 
       };
 
       const response = await axios.post('https://backend-tienda-mac-production.up.railway.app/api/openpay/create-charge', paymentData);
