72ea9f34efcd42ece2b27ce5994ddd0ea5fed7c8
Daniel Amaya <danijcd.com@gmail.com>
2024-09-09 23:09:57 -0500

    soporte

--- src/components/MenuDashboard/MenuDashboard.jsx
+++ src/components/MenuDashboard/MenuDashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUsers, faShoppingBasket, faTicketAlt, faTrademark, faListAlt, faPalette, faCubes, faCheckSquare, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faUsers, faShoppingBasket, faTicketAlt, faTrademark, faListAlt, faPalette, faCubes, faCheckSquare, faShoppingCart, faTools } from '@fortawesome/free-solid-svg-icons'; // Asegúrate de importar el ícono adecuado
 import { NavDropdown } from 'react-bootstrap';
 import './MenuDashboard.css'; // Asegúrate de que este importe apunta al archivo CSS correcto
 
@@ -10,6 +10,34 @@ const MenuDashboard = () => {
         <nav id="sidebar" className="col-md-3 col-lg-2 d-md-block bg-dark sidebar">
             <div className="position-sticky">
                 <ul className="nav flex-column">
+                    {/* <li className="nav-item">
+                        <NavLink to="/coupons" className="nav-link text-white">
+                            <FontAwesomeIcon icon={faTicketAlt} className="me-2" />
+                            Cupones
+                        </NavLink>
+                    </li> */}
+                    <li className="nav-item">
+                        <NavLink to="/admin/purchases" className="nav-link text-white">
+                            <FontAwesomeIcon icon={faShoppingCart} className="me-2" />
+                            Compras
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/soporte-tecnico" className="nav-link text-white">
+                            <FontAwesomeIcon icon={faTools} className="me-2" />
+                            Soporte Técnico
+                        </NavLink>
+                    </li>
+                    <NavDropdown title={<><FontAwesomeIcon icon={faUsers} className="me-2" />Usuarios</>} id="basic-nav-dropdown">
+                        <NavDropdown.Item as={NavLink} to="/useradmin" className="text-dark">
+                            <FontAwesomeIcon icon={faUsers} className="me-2" />
+                            Administradores
+                        </NavDropdown.Item>
+                        <NavDropdown.Item as={NavLink} to="/client" className="text-dark">
+                            <FontAwesomeIcon icon={faUsers} className="me-2" />
+                            Clientes
+                        </NavDropdown.Item>
+                    </NavDropdown>
                     <li className="nav-item">
                         <NavLink to="/product" className="nav-link text-white">
                             <FontAwesomeIcon icon={faShoppingBasket} className="me-2" />
@@ -46,40 +74,13 @@ const MenuDashboard = () => {
                             Capacidad
                         </NavLink>
                     </li>
-                    <NavDropdown title={<><FontAwesomeIcon icon={faUsers} className="me-2" />Usuarios</>} id="basic-nav-dropdown">
-                        <NavDropdown.Item as={NavLink} to="/useradmin" className="text-dark">
-                            <FontAwesomeIcon icon={faUsers} className="me-2" />
-                            Administradores
-                        </NavDropdown.Item>
-                        <NavDropdown.Item as={NavLink} to="/client" className="text-dark">
-                            <FontAwesomeIcon icon={faUsers} className="me-2" />
-                            Clientes
-                        </NavDropdown.Item>
-                    </NavDropdown>
                     <li className="nav-item">
                         <NavLink to="/brands" className="nav-link text-white">
                             <FontAwesomeIcon icon={faTrademark} className="me-2" />
                             Marcas
                         </NavLink>
                     </li>
-                    <li className="nav-item">
-                        <NavLink to="/orders" className="nav-link text-white">
-                            <FontAwesomeIcon icon={faShoppingBasket} className="me-2" />
-                            Órdenes
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/coupons" className="nav-link text-white">
-                            <FontAwesomeIcon icon={faTicketAlt} className="me-2" />
-                            Cupones
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/admin/purchases" className="nav-link text-white">
-                            <FontAwesomeIcon icon={faShoppingCart} className="me-2" />
-                            Compras
-                        </NavLink>
-                    </li>
+                    
                 </ul>
             </div>
         </nav>
