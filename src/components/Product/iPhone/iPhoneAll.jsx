a47e71ef2b3a5b8a4949e18432903f4327591012
Daniel Amaya <danijcd.com@gmail.com>
2024-09-28 14:39:33 -0500

    Todo Ok

--- src/components/Product/iPhone/iPhoneAll.jsx
+++ src/components/Product/iPhone/iPhoneAll.jsx
@@ -41,7 +41,7 @@ const IphoneAll = () => {
     <div className="iphone-products">
       <Navbar />
       <div className="container py-5">
-        <h1 className="text-center mb-4 fs-4">Productos iPhone</h1>
+        <h1 className="text-center mb-4 fs-4">iPhones</h1>
         <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
           {iphoneProducts.map((product) => (
             <div className="col" key={product.id}>
