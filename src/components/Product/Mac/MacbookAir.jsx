859c7f3832a9d7a442bf090dfbba817da27e1494
Daniel Amaya <danijcd.com@gmail.com>
2024-11-03 02:50:46 -0500

    Frontend_3/11/2024_Sonido_Images

--- src/components/Product/Mac/MacbookAir.jsx
+++ src/components/Product/Mac/MacbookAir.jsx
@@ -18,23 +18,16 @@ const MacbookAir = () => {
         const products = responses.flatMap(response => response.data);
         setMacProducts(products);
 
-        // Fetch all product images after setting products
-        const imageFetchPromises = products.map(product =>
-          axios.get(`https://backend-tienda-mac-production.up.railway.app/products/${product.id}/images`)
-            .then(imageResponse => {
-              const imageFileNames = imageResponse.data;
-              const imageUrls = imageFileNames.map(fileName => `https://backend-tienda-mac-production.up.railway.app/images/${fileName}`);
-              return { id: product.id, images: imageUrls };
-            })
-        );
-
-        const images = await Promise.all(imageFetchPromises);
-        const imagesObject = images.reduce((acc, { id, images }) => {
-          acc[id] = images;
-          return acc;
-        }, {});
-
-        setProductImages(imagesObject);
+        products.forEach(async (product) => {
+          try {
+            const imageResponse = await axios.get(`https://backend-tienda-mac-production.up.railway.app/products/${product.id}/images`);
+            const imageFileNames = imageResponse.data;
+            const imageUrls = imageFileNames.map(fileName => `https://backend-tienda-mac-production.up.railway.app/images/${fileName}`);
+            setProductImages(prevState => ({ ...prevState, [product.id]: imageUrls }));
+          } catch (error) {
+            console.error(`Error getting images for product ${product.id}:`, error);
+          }
+        });
       } catch (error) {
         console.error('Error fetching Mac products:', error);
       }
