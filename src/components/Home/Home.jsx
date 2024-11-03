103358eddb11a02a47db9c6d64ea7f1a1505767f
Daniel Amaya <danijcd.com@gmail.com>
2024-11-03 16:23:09 -0500

    Frontend_3/11/2024_Mac_Images

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
 import styles from './Home.module.css';
 import Footer from '../Footer/Footer';
 import Slideshow from './Slideshow';
@@ -18,82 +17,131 @@ const Home = () => {
   const [productImages, setProductImages] = useState({});
   const [newProductImages, setNewProductImages] = useState({});
   const [featuredProductImages, setFeaturedProductImages] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
-
-  // Array de URLs de categorías de accesorios
-  const accessoryUrls = [
-    'https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20TV/subcategory/Controles%20remotos',
-    'https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Cargador%20MagSafe',
-    'https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20reloj/subcategory/Protector%20de%20pantalla%20para%20Apple%20Watch',
-    'https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20Grabación%20y%20soporte%20de%20teléfono/subcategory/Soporte%20magnético%20girable%20para%20grabación',
-  ];
-
-  const fetchImagesBatch = async (products, setImageState, batchSize = 5) => {
-    const batches = [];
-    for (let i = 0; i < products.length; i += batchSize) {
-      batches.push(products.slice(i, i + batchSize));
-    }
-
-    for (const batch of batches) {
-      const imageFetchPromises = batch.map(async (product) => {
-        try {
-          const imageResponse = await axios.get(`https://backend-tienda-mac-production.up.railway.app/products/${product.id}/images`);
-          if (imageResponse.data && imageResponse.data.length > 0) {
-            const base64Images = imageResponse.data.map(image => {
-              if (image && image.data) {
-                return `data:image/jpeg;base64,${image.data}`;
-              }
-              return null;
-            }).filter(img => img !== null);
-            
-            if (base64Images.length > 0) {
-              setImageState(prevState => ({ ...prevState, [product.id]: base64Images }));
-            }
-          }
-        } catch (error) {
-          console.error(`Error getting images for product ${product.id}:`, error);
-        }
-      });
-
-      await Promise.all(imageFetchPromises);
-    }
-  };
 
   useEffect(() => {
-    const fetchAllData = async () => {
-      setIsLoading(true);
+    const fetchHomeProducts = async () => {
       try {
-        const accessoryResponses = await Promise.all(
-          accessoryUrls.map(url => axios.get(url))
-        );
-        const allAccessories = accessoryResponses.flatMap(response => response.data);
-        
-        const [newProductsResponse, featuredResponse] = await Promise.all([
-          axios.get('https://backend-tienda-mac-production.up.railway.app/products/recent'),
-          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Smartphones/subcategory/iPhone')
+        const responses = await Promise.all([
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20TV/subcategory/Controles%20remotos'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Cargador%20MagSafe'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Audífonos/subcategory/Audífonos%20de%20cable'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Adaptadores/subcategory/Adaptador%20VGA'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Adaptadores/subcategory/Adaptador%20de%20audio'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20computación/subcategory/Mouse'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Cable%20de%20carga%20magnetica'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Cargador%20de%20coche%20con%204%20puertos%20USB'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Cargador%20de%202%20puertos%20USB%20para%20Coche%20+%20Cable%20Lightning%20a%20USB'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Cargador%20de%20coche%20USB%20+%20cable%20lightning'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Cargador%20de%20carro%20de%202%20puertos%20con%20cable%20usb-a%20con%20conector%20lightning'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Cargador%20dual-USB-A%20para%20coche'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Cargador%20de%20coche%20dual%20USB-C+%20USB-A'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Cargador%20Universal%20para%20coche'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Soporte%20de%20viaje%20para%20el%20cable%20de%20carga%20y%20el%20%20Apple%20Watch'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Base%20de%20carga%202%20en%201%20para%20iPhone%20y%20Apple%20Watch'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Cargador%20de%20carro%20+%20cable%20lightning%20a%20USB'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Cargador%20Clip%20de%20puerto%20Lightning%20a%20USB'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Cable%20de%20carga%20USB-A%20a%20Lightning'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Cable%20de%20carga%20USB-C%20a%20Lightning'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Cable%20de%20carga%20USB%20con%20Adaptador%20Lightning'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Cargador%20de%20pared'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Cargador%20de%20carro%20+%20cable%20lightning'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Cargador%20de%20pared%20de%20puerto%20USB'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Cargador%20de%20coche%20con%20doble%20puerto%20USB-A'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Cargador%20de%204%20puertos'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Base%20de%20carga%20para%20iPhone%20y%20apple%20watch'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Soporte%20de%20carga%20inalámbrica%20para%20teléfonos'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20video/subcategory/Adaptador%20USB-C%20a%20HDMI'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Bateria%20Portátil'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga/subcategory/Batería%20externa,%20inalámbrica%20y%20magnética%20con%20soporte'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga%20y%20navegación/subcategory/Soporte%20de%20carga%20para%20teléfono%20móvil%20+%20Navegación%20para%20automóvil'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga%20para%20apple%20watch%20y%20iPhone/subcategory/Bateria%20portátil'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Cables%20de%20Audio%20y%20Video/subcategory/Cable%20HD-HDMI'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20deportivos/subcategory/Brazalete%20deportivo%20+%20Estuche%20de%20seguridad'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20deportivos/subcategory/Banda%20de%20mano%20protectora%20para%20iPhone'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20deportivos/subcategory/Brazalete%20deportivo%20para%20iPhone'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20Audio%20o%20Sonido/subcategory/Audífonos%20para%20niños'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carro/subcategory/Soporte%20de%20carro%20para%20teléfono%20móvil'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Cables%20de%20imagen/subcategory/Adaptador%20Mini%20Displayport%20a%20VGA'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Cables%20de%20imagen/subcategory/Adaptador%20usb-c%20a%20Vga'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20reloj/subcategory/Protector%20de%20pantalla%20para%20Apple%20Watch'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20Grabación%20y%20soporte%20de%20teléfono/subcategory/Soporte%20magnético%20girable%20para%20grabación'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20Audio%20o%20Sonido/subcategory/Cable%20de%20audio%20con%20conector%20lightning'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20Audio%20o%20Sonido/subcategory/Distribuidor%20de%20audio'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga%20y%20transferencia%20de%20datos/subcategory/Cable%20USB-C%20a%20USB-C'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga%20y%20transferencia%20de%20datos/subcategory/Llavero%20con%20puerto%20lightning%20a%20USB'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga%20y%20transferencia%20de%20datos/subcategory/Cable%20Lightning%20a%20USB-C'),
+          axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Accesorios%20de%20carga%20y%20transferencia%20de%20datos/subcategory/Cable%20USB-C%20a%20Lightning')
         ]);
 
-        const newProds = newProductsResponse.data.slice(0, 10);
-        const featured = featuredResponse.data.slice(0, 10);
+        const products = responses.flatMap(response => response.data);
+        setHomeProducts(products);
 
-        setHomeProducts(allAccessories);
-        setNewProducts(newProds);
-        setFeaturedProducts(featured);
-
-        await Promise.all([
-          fetchImagesBatch(allAccessories, setProductImages),
-          fetchImagesBatch(newProds, setNewProductImages),
-          fetchImagesBatch(featured, setFeaturedProductImages)
-        ]);
-
+        products.forEach(async (product) => {
+          try {
+            const imageResponse = await axios.get(`https://backend-tienda-mac-production.up.railway.app/products/${product.id}/images`);
+            const imageFileNames = imageResponse.data;
+            const imageUrls = imageFileNames.map(fileName => `https://backend-tienda-mac-production.up.railway.app/images/${fileName}`);
+            setProductImages(prevState => ({ ...prevState, [product.id]: imageUrls }));
+          } catch (error) {
+            console.error(`Error getting images for product ${product.id}:`, error);
+          }
+        });
       } catch (error) {
-        console.error('Error fetching data:', error);
-      } finally {
-        setIsLoading(false);
+        console.error('Error fetching products:', error);
       }
     };
 
-    fetchAllData();
+    fetchHomeProducts();
+  }, []);
+
+  useEffect(() => {
+    const fetchNewProducts = async () => {
+      try {
+        const response = await axios.get('https://backend-tienda-mac-production.up.railway.app/products/recent');
+        const newProducts = response.data;
+        setNewProducts(newProducts);
+
+        newProducts.forEach(async (product) => {
+          try {
+            const imageResponse = await axios.get(`https://backend-tienda-mac-production.up.railway.app/products/${product.id}/images`);
+            const imageFileNames = imageResponse.data;
+            const imageUrls = imageFileNames.map(fileName => `https://backend-tienda-mac-production.up.railway.app/images/${fileName}`);
+            setNewProductImages(prevState => ({ ...prevState, [product.id]: imageUrls }));
+          } catch (error) {
+            console.error(`Error getting images for new product ${product.id}:`, error);
+          }
+        });
+      } catch (error) {
+        console.error('Error fetching new products:', error);
+      }
+    };
+
+    fetchNewProducts();
+  }, []);
+
+  useEffect(() => {
+    const fetchFeaturedProducts = async () => {
+      try {
+        const response = await axios.get('https://backend-tienda-mac-production.up.railway.app/products/category/Smartphones/subcategory/iPhone');
+        const products = response.data;
+        setFeaturedProducts(products);
+
+        products.forEach(async (product) => {
+          try {
+            const imageResponse = await axios.get(`https://backend-tienda-mac-production.up.railway.app/products/${product.id}/images`);
+            const imageFileNames = imageResponse.data;
+            const imageUrls = imageFileNames.map(fileName => `https://backend-tienda-mac-production.up.railway.app/images/${fileName}`);
+            setFeaturedProductImages(prevState => ({ ...prevState, [product.id]: imageUrls }));
+          } catch (error) {
+            console.error(`Error getting images for featured product ${product.id}:`, error);
+          }
+        });
+      } catch (error) {
+        console.error('Error fetching featured products:', error);
+      }
+    };
+
+    fetchFeaturedProducts();
   }, []);
 
   const swiperParams = {
@@ -110,73 +158,29 @@ const Home = () => {
     }
   };
 
-  const renderProductCard = (product, images) => {
-    const productImages = images[product.id] || [];
-    const hasValidImage = productImages.length > 0;
-
-    return (
-      <div className="card h-100 border-0 shadow-sm" style={{ 
-        maxWidth: '300px', 
-        margin: '0 auto', 
-        backgroundColor: 'white' 
-      }}>
-        <div className="d-flex align-items-center justify-content-center" style={{ 
-          height: '200px', 
-          overflow: 'hidden' 
-        }}>
-          {hasValidImage ? (
-            <LazyLoadImage
-              src={productImages[0]}
-              alt={product.name}
-              effect="blur"
-              className="card-img-top img-fluid"
-              style={{ 
-                maxWidth: '100%',
-                maxHeight: '100%',
-                objectFit: 'contain'
-              }}
-              placeholder={
-                <div className="placeholder-glow w-100 h-100 bg-light d-flex align-items-center justify-content-center">
-                  <div className="spinner-border text-primary" role="status">
-                    <span className="visually-hidden">Cargando...</span>
-                  </div>
-                </div>
-              }
-              error={
-                <div className="d-flex align-items-center justify-content-center h-100 w-100 bg-light">
-                  <span className="text-muted">Imagen no disponible</span>
-                </div>
-              }
-            />
-          ) : (
-            <div className="d-flex align-items-center justify-content-center h-100 w-100 bg-light">
-              <span className="text-muted">Imagen no disponible</span>
-            </div>
-          )}
-        </div>
-        <div className="card-body text-center flex-grow-1 d-flex flex-column justify-content-between p-3">
-          <h6 className="card-title text-truncate mb-2" style={{ fontSize: '1.1rem' }}>{product.name}</h6>
-          <p className="card-text mb-3" style={{ fontSize: '1rem' }}>
-            {new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(product.price)}
-          </p>
-          <a href={`/detalle-producto/${product.id}`} className="btn btn-primary">Comprar</a>
-        </div>
+  const renderProductCard = (product, images) => (
+    <div className="card h-100 border-0 shadow-sm" style={{ maxWidth: '300px', margin: '0 auto', backgroundColor: 'white' }}>
+      <div className="d-flex align-items-center justify-content-center" style={{ height: '200px', overflow: 'hidden' }}>
+        <img src={images[product.id]?.[0]} className="card-img-top img-fluid" alt={product.name} style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'contain' }} />
       </div>
-    );
-  };
-
-  if (isLoading) {
-    return (
-      <div className="d-flex justify-content-center align-items-center min-vh-100">
-        <div className="spinner-border text-primary" role="status">
-          <span className="visually-hidden">Cargando...</span>
-        </div>
+      <div className="card-body text-center flex-grow-1 d-flex flex-column justify-content-between p-3">
+        <h6 className="card-title text-truncate mb-2" style={{ fontSize: '1.1rem' }}>{product.name}</h6>
+        <p className="card-text mb-3" style={{ fontSize: '1rem' }}>{new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(product.price)}</p>
+        <a href={`/detalle-producto/${product.id}`} className="btn btn-primary">Comprar</a>
       </div>
-    );
-  }
+    </div>
+  );
+
+  // Estilo en línea para ocultar los puntos de paginación
+  const hidePaginationStyle = `
+    .swiper-pagination-bullets {
+      display: none !important;
+    }
+  `;
 
   return (
     <div className={styles.homeContainer}>
+      <style>{hidePaginationStyle}</style>
       <Slideshow />
       <div className="container-fluid py-5">
         <section className="mb-5">
