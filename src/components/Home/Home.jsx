c9bfd240945fc197b2a9b22bbd88a3faec649626
Daniel Amaya <danijcd.com@gmail.com>
2024-11-08 00:20:51 -0500

    Frontend_7/11/2024

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -256,7 +256,7 @@ const Home = () => {
               }}
             >
               <img
-                src="../../img/slidesShow/Soporte_Tecnico.jpg"
+                src="/img/slidesShow/Soporte_Tecnico.jpg"
                 alt="Soporte Técnico"
                 style={{
                   width: '100%',
