c5b3098582e5543129767b7e8faa263826ad7ebf
Daniel Amaya <danijcd.com@gmail.com>
2024-11-22 18:41:08 -0500

    accesorios varios

--- src/components/Home/SectionsProducts/AccessoriesSection.jsx
+++ src/components/Home/SectionsProducts/AccessoriesSection.jsx
@@ -45,6 +45,23 @@ const AccessoriesSection = () => {
           "Accesorios de carga/Cargador MagSafe",
           "Audífonos/Audífonos de cable",
           "Adaptadores/Adaptador VGA",
+          "Adaptadores/Adaptador de audio",
+          "Accesorios de computación/Mouse",
+          "Accesorios de carga/Cable de carga magnetica",
+          "Accesorios de carga/Cargador de coche con 4 puertos USB",
+          "Accesorios de carga/Cargador de 2 puertos USB para Coche + Cable Lightning a USB",
+          "Accesorios de carga/Cargador de coche USB + cable lightning",
+          "Accesorios de carga/Cargador de carro de 2 puertos con cable usb-a con conector lightning",
+          "Accesorios de carga/Cargador dual-USB-A para coche",
+          "Accesorios de carga/Cargador de coche dual USB-C+ USB-A",
+          "Accesorios de carga/Cargador Universal para coche",
+          "Accesorios de carga/Soporte de viaje para el cable de carga y el Apple Watch",
+          "Accesorios de carga/Base de carga 2 en 1 para iPhone y Apple Watch",
+          "Accesorios de carga/Cargador de carro + cable lightning a USB",
+          "Accesorios de carga/Cargador Clip de puerto Lightning a USB",
+          "Accesorios de carga/Cable de carga USB-A a Lightning",
+          "Accesorios de carga/Cable de carga USB-C a Lightning",
+          "Accesorios de carga/Cable de carga USB con Adaptador Lightning",
         ].map((subcategory) => {
           const [category, subcategoryName] = subcategory.split("/");
           return api.get(
